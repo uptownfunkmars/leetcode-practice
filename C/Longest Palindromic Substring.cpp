d3ac9a7ea5cbcd2aee5fec2f2459142e92b5555b
BossJia <2762635664@qq.com>
2019-11-20 14:06:40 +0800

    Update Longest Palindromic Substring.cpp

--- C/Longest Palindromic Substring.cpp
+++ C/Longest Palindromic Substring.cpp
@@ -1,11 +1,15 @@
+char *substr_ = NULL;
 char * longestPalindrome(char * s) {
+    char substr[10100] = {0};
 	int len = strlen(s);
-
+    
+    printf("len = %d ", len);
+    
 	int k = 0;
-	int sub_left, sub_right;
+	int sub_left = 0, sub_right = 0;
 	int max_len = 0;
-	static char substr[1000] = { 0 };
-
+    //static char substr[1000] = {0};
+    
 	while (k < len) {
 		int i = k - 1;
 		int j = k + 1;
@@ -34,6 +38,7 @@ char * longestPalindrome(char * s) {
 			if (j - i + 1 > max_len) {
 				sub_left = i;
 				sub_right = j;
+                max_len = j - i + 1;
 			}
 			i--;
 			j++;
@@ -49,6 +54,7 @@ char * longestPalindrome(char * s) {
 			if (j - i + 1 > max_len) {
 				sub_left = i;
 				sub_right = j;
+                max_len = j - i + 1;
 			}
 			i--;
 			j++;
@@ -56,11 +62,15 @@ char * longestPalindrome(char * s) {
 
 		k++;
 	}
-
+    
 	for (int p = sub_left; p <= sub_right; p++) {
 		substr[p - sub_left] = s[p];
 	}
-    // printf("substrlen = %d\n", strlen(substr));
-    // puts(substr);
-	return substr;
+    
+    printf("sublen = %d\n", strlen(substr));
+    
+    substr_ = substr;
+    
+    return substr_;
 }
+
